414a29c4675283329aaab0077bf959b72d08260d
Artem <artemmilov@gmail.com>
2022-03-01 01:18:59 +0300

    Add multi metrics 3

--- abstract/abstract_tool.py
+++ abstract/abstract_tool.py
@@ -3,7 +3,7 @@ import os
 import shutil
 from collections import defaultdict
 
-from general import transform_from_to
+import general
 from report import write_report
 
 
@@ -47,7 +47,7 @@ class AbstractTool:
         if len(database_name.split('.')) != 2:
             raise ValueError('Indefinable format')
         cur_format = database_name.split('.')[1]
-        transform_from_to(
+        general.transform_from_to(
             cur_format,
             self._database_format,
             database,
@@ -70,7 +70,7 @@ class AbstractTool:
             if len(spectra.split('.')) != 2:
                 raise ValueError('Indefinable format')
             cur_format = spectra.split('.')[1]
-            transform_from_to(
+            general.transform_from_to(
                 cur_format,
                 self._spectra_format,
                 os.path.join(spectres, spectra),
@@ -102,33 +102,20 @@ class AbstractTool:
         self._parse_output(abs_folder, os.path.split(challenge)[-1])
 
     def _make_conclusion(self, abs_folder):
-        true_answers = {}
-        tool_answers = defaultdict(list)
-        true_answers_data_file = os.path.join(
-            abs_folder,
-            'true_answers.txt',
+        true_answers = general.parse_true_answers(
+            os.path.join(
+                abs_folder,
+                'true_answers.txt',
+            ),
         )
-        with open(true_answers_data_file) as true_answers_data:
-            for true_answer in true_answers_data.read().split('\n'):
-                if true_answer != '':
-                    true_answers[true_answer.split('\t')[0]] =\
-                        true_answer.split('\t')[1]
-        tool_answers_data_file = \
+        tool_answers = general.parse_tool_answers(
             os.path.join(
                 abs_folder,
                 'reports',
                 self._tool_name,
                 'tool_answers.txt',
-            )
-        with open(tool_answers_data_file) as tool_answers_data:
-            for tool_answer in tool_answers_data.read().split('\n'):
-                if tool_answer != '':
-                    tool_answers[tool_answer.split('\t')[0]].append(
-                        (
-                            tool_answer.split('\t')[1],
-                            tool_answer.split('\t')[2],
-                        ),
-                    )
+            ),
+        )
         write_report(
             os.path.join(
                 abs_folder,
